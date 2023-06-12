9e0d22d3dc6a755f4ed1f6e0fc847c77adc5e03c
Paula Acosta <97809361+acosta604@users.noreply.github.com>
2023-06-12 16:01:01 -0500

    Ejercicios clase 7

---
+++ clase 7/Ejercicio_1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EjercicioUno{
+
+	public static void main(String[] args) {		 
+
+		 int[] numeros = new int[5];
+
+		 System.out.println("Ingrese 5 numeros");
+
+		 
+
+		Scanner sc = new Scanner(System.in);
+
+		
+
+		for (int i = 0; i < numeros.length; i++) {
+
+     System.out.println("ingrese numero "+ (i+1));
+
+            numeros[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Numeros ingresados:");
+
+        for (int numero: numeros) {
+
+            System.out.println(numero);
+
+        }
+
+		  
+
+		  
+
+		
+
+	}
+
+}
