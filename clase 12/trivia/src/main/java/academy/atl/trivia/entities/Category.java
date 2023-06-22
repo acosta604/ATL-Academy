b120c07e55b8cfa471b49d7691f17a09f30475dc
Paula Acosta <97809361+acosta604@users.noreply.github.com>
2023-06-22 17:14:52 -0500

    Ejercicio clase 12

---
+++ clase 12/trivia/src/main/java/academy/atl/trivia/entities/Category.java
@@ -0,0 +1,25 @@
+package academy.atl.trivia.entities;
+
+public class Category {
+
+    private String category;
+    private String description;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
